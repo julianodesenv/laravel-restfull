720f4fab3308ab8d8cd8f9905c1c3729332ca87f
julianodesenv <julianodesenv@gmail.com>
2020-03-16 20:29:28 -0300

    adicionando cache

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductsRequest;
 use App\Model\Product;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
@@ -11,11 +12,16 @@ class ProductController extends Controller
 
     public function index()
     {
-        return Product::all();
+        $minutes = Carbon::now()->addMinutes(10);
+        $products = \Cache::remember('api::products', $minutes, function (){
+            return Product::all();
+        });
+        return $products;
     }
 
     public function store(ProductsRequest $request)
     {
+        \Cache::forget('api::products');
         $data = $request->all();
         $data['user_id'] = Auth::user()->id;
         return Product::create($data);
